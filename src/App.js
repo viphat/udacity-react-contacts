b60f8c30eb84177cbc19b747f23be6c63ff64b7f
Duong Vi Phat <viphat@gmail.com>
2018-05-03 06:30:55 +0700

    Lesson 5.6 - Finishing the Contact Form

--- src/App.js
+++ src/App.js
@@ -29,6 +29,15 @@ class App extends Component {
     ContactsAPI.remove(contact);
   };
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).
+      then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
+
   render() {
     return (
       <div>
@@ -38,7 +47,14 @@ class App extends Component {
             onDeleteContact={this.removeContact}
           />
         )} />
-        <Route path='/create' component={CreateContact} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact);
+              history.push('/');
+            }}
+          />
+        )} />
       </div>
     );
   }
